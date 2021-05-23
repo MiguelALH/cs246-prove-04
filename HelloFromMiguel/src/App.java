23feb0738f11afeecff87cde96b6d05c46bd8b6d
Lucila Guzman Mijangos <78246446+LucilaGM@users.noreply.github.com>
2021-05-23 00:22:17 -0500

    Update App.java

--- HelloFromMiguel/src/App.java
+++ HelloFromMiguel/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, my name is Miguel");
         System.out.println("Hello, Miguel, my name is Lucila.");
+        System.out.println("Hello, Miguel, my name is Lucila... Again!");
     }
 }
